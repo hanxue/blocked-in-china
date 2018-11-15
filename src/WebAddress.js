0c2fcb85f70eb71ea46cb5dd59df805f9644a909
Lee Hanxue <hanxue.developer@gmail.com>
2018-11-15 17:03:15 +0800

    Add Autocomplete for searching domain/URL
    - Give 10 most popular URLs
    - Use deburr, downshift and keycode libraries for the autocomplete

--- src/WebAddress.js
+++ src/WebAddress.js
@@ -1,60 +1,181 @@
 import React from 'react';
-import AutoComplete from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import deburr from 'lodash/deburr';
+import keycode from 'keycode';
+import Downshift from 'downshift';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
-const dataSource2 = [
-  'facebook.com',
-  'wikileaks.ch',
-  'youtube.com',
-  'twitter.com',
-  'google.com',
-  'gmail.com',
-  'cnn.com',
-  'wikipedia.org',
-  'dropbox.com',
-  'yahoo.com',
+const suggestions = [
+  { label: 'facebook.com' },
+  { label: 'wikileaks.ch' },
+  { label: 'twitter.com' },
+  { label: 'youtube.com' },
+  { label: 'google.com' },
+  { label: 'gmail.com' },
+  { label: 'cnn.com' },
+  { label: 'wikipedia.org' },
+  { label: 'dropbox.com' },
+  { label: 'yahoo.com' },
 ];
 
+function renderInput(inputProps) {
+  const { InputProps, classes, ref, ...other } = inputProps;
+
+  return (
+    <TextField
+      InputProps={{
+        inputRef: ref,
+        classes: {
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        },
+        ...InputProps,
+      }}
+      {...other}
+    />
+  );
+}
+
+function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem }) {
+  const isHighlighted = highlightedIndex === index;
+  const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1;
+
+  return (
+    <MenuItem
+      {...itemProps}
+      key={suggestion.label}
+      selected={isHighlighted}
+      component="div"
+      style={{
+        fontWeight: isSelected ? 500 : 400,
+      }}
+    >
+      {suggestion.label}
+    </MenuItem>
+  );
+}
+renderSuggestion.propTypes = {
+  highlightedIndex: PropTypes.number,
+  index: PropTypes.number,
+  itemProps: PropTypes.object,
+  selectedItem: PropTypes.string,
+  suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired,
+};
+
+function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
+
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
+        const keep =
+          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+}
+
 const styles = theme => ({
-  webAddress: {
+  root: {
+    flexGrow: 1,
+    height: 250,
     paddingTop: theme.spacing.unit * 5,
     paddingBottom: theme.spacing.unit * 5,
     paddingLeft: theme.spacing.unit * 5,
     paddingRight: theme.spacing.unit * 5,
   },
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  inputRoot: {
+    flexWrap: 'wrap',
+  },
+  inputInput: {
+    width: 'auto',
+    flexGrow: 1,
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
 });
 
-/**
- * The first example has `MenuItem`s in its data source that display on data entry.
- * The second example uses an array of values as its `dataSource`, and updates on focus.
- * Both examples have filtering disabled.
- */
 function WebAddress(props) {
   const { classes } = props;
 
   return (
-  <div class={classes.webAddress}>
-    <TextField
-      floatingLabelText="Key in web address here"
-      filter={AutoComplete.noFilter}
-      openOnFocus={true}
-      dataSource={dataSource2}
-    /><br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <Typography variant="h3" gutterBottom>
-        Is website.com accessible?
-    </Typography>
-    <Typography variant="h4" gutterBottom>
-      Yes, website.com is accessible
-    </Typography>
-  </div>
-  )
+    <div className={classes.root}>
+      <Downshift id="downshift-simple">
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          highlightedIndex,
+          inputValue,
+          isOpen,
+          selectedItem,
+        }) => (
+          <div className={classes.container}>
+            {renderInput({
+              fullWidth: true,
+              classes,
+              InputProps: getInputProps({
+                placeholder: 'Check a website or URL',
+              }),
+            })}
+            <div {...getMenuProps()}>
+              {isOpen ? (
+                <Paper className={classes.paper} square>
+                  {getSuggestions(inputValue).map((suggestion, index) =>
+                    renderSuggestion({
+                      suggestion,
+                      index,
+                      itemProps: getItemProps({ item: suggestion.label }),
+                      highlightedIndex,
+                      selectedItem,
+                    }),
+                  )}
+                </Paper>
+              ) : null}
+            </div>
+          </div>
+        )}
+      </Downshift>
+      <div className={classes.divider} />
+      <div className={classes.divider} />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Typography variant="h3" gutterBottom>
+          Is website.com accessible?
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        Yes, website.com is accessible
+      </Typography>
+    </div>
+  );
 };
 
 WebAddress.propTypes = {
