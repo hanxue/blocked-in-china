3a4d5ab5dca14221ca6158ebd31698046863867b
Lee Hanxue <hanxue.developer@gmail.com>
2018-11-15 21:48:28 +0800

    Updates

---
+++ precache-manifest.3d1c3b2e35bffd97a651694cf03b789a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a164245f66c0002d3d11",
+    "url": "/blocked-in-china/static/js/runtime~main.a164245f.js"
+  },
+  {
+    "revision": "3f721ff2ae5a9a5581de",
+    "url": "/blocked-in-china/static/js/main.3f721ff2.chunk.js"
+  },
+  {
+    "revision": "4583ebffca710cc398d1",
+    "url": "/blocked-in-china/static/js/1.4583ebff.chunk.js"
+  },
+  {
+    "revision": "3f721ff2ae5a9a5581de",
+    "url": "/blocked-in-china/static/css/main.93d3f0f0.chunk.css"
+  },
+  {
+    "revision": "56f6ed1b35a91c9a921fc3425947413b",
+    "url": "/blocked-in-china/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f93c2f9796e75b68bb997b4be440336.js"
+  "/blocked-in-china/precache-manifest.3d1c3b2e35bffd97a651694cf03b789a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blocked-in-china/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
